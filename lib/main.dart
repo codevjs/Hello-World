6973a3e18d0daecd01b0c86f4753d1d7e38cd3c3
neutron <codevjs@gmail.com>
2021-02-21 15:26:31 +0800

    Column & row

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title : "Selamat datang",
-      home  : Scaffold(
-        appBar: AppBar(
-          title: Text("Aplikasi Pertama Saya"),
-        ),
-        body: Center(
-          child: Text("Hello world"),
-        ),
-      )
-    );
+        title: "Selamat Datang",
+        home: Scaffold(
+          appBar: AppBar(title: Text("Hello World")),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Hello World 1"),
+              Text("Hello World 2"),
+              Text("Hello World 3"),
+              Row(
+                children: [
+                  Text("Hello World 4"),
+                  Text("Hello World 5"),
+                  Text("Hello World 6"),
+                ],
+              )
+            ],
+          )
+        ));
   }
 }
